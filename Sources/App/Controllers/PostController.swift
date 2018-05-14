1f1714f31ab147b69a95ddcdd1eac1e2be56efaa
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-14 11:17:04 +0900

    Refactor controller using computed properties

--- Sources/App/Controllers/PostController.swift
+++ Sources/App/Controllers/PostController.swift
@@ -2,18 +2,25 @@ import Foundation
 import Vapor
 
 final class PostController {
-    func index(_ req: Request) throws -> Future<View> {
-        let directoryConfig = try req.make(DirectoryConfig.self)
-        let postSourceDir = URL(fileURLWithPath: "\(directoryConfig.workDir)Public/post-sources")
-
-        let fm = FileManager()
-
-        guard let files = try? fm.contentsOfDirectory(at: postSourceDir, includingPropertiesForKeys: nil) else {
-            throw Abort(.internalServerError)
+    var postSourceDir: URL {
+        get {
+            let directoryConfig = DirectoryConfig.detect()
+            return URL(fileURLWithPath: "\(directoryConfig.workDir)Public/post-sources")
         }
+    }
 
-        let visibleFiles = files.filter { !$0.lastPathComponent.hasPrefix(".") }
+    var postSourceFiles: [URL] {
+        get {
+            let fm = FileManager()
+            guard let files = try? fm.contentsOfDirectory(at: postSourceDir, includingPropertiesForKeys: nil) else {
+                return []
+            }
+            return files
+        }
+    }
 
+    func index(_ req: Request) throws -> Future<View> {
+        let visibleFiles = postSourceFiles.filter { !$0.lastPathComponent.hasPrefix(".") }
         var posts: [Post] = []
 
         for file in visibleFiles {
@@ -23,34 +30,18 @@ final class PostController {
             posts.append(post)
         }
 
-        let context = [
-            "posts": posts
-        ]
+        let context = ["posts": posts]
         return try req.view().render("posts/index", context)
     }
 
     func show(_ req: Request) throws -> Future<View> {
         let postName = try req.parameters.next(String.self)
-
-        let directoryConfig = try req.make(DirectoryConfig.self)
-        let postSourceDir = URL(fileURLWithPath: "\(directoryConfig.workDir)Public/post-sources")
-
-        let fm = FileManager()
-
-        guard let files = try? fm.contentsOfDirectory(at: postSourceDir, includingPropertiesForKeys: nil) else {
-            throw Abort(.internalServerError)
-        }
-
-        let targetFile = files.filter { $0.lastPathComponent == "\(postName).md" }.first
+        let targetFile = postSourceFiles.filter { $0.lastPathComponent == "\(postName).md" }.first
 
         if let targetFile = targetFile {
             let contents = try String(contentsOf: targetFile)
             let post = try Post.parseRawContents(of: contents)
-
-            let context = [
-                "post": post
-            ]
-
+            let context = ["post": post]
             return try req.view().render("posts/show", context)
         } else {
             throw Abort(.internalServerError)
