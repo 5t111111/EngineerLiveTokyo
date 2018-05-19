90eed3b7d6069b77d156453b053f0f4bac123991
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-19 20:10:06 +0900

    Use Env var for api endpoint

--- Sources/App/FrontEnd/Controllers/PostViewController.swift
+++ Sources/App/FrontEnd/Controllers/PostViewController.swift
@@ -2,7 +2,7 @@ import Vapor
 
 final class PostViewController {
     func index(_ req: Request) throws -> Future<View> {
-        return try req.client().get("http://localhost:8080/api/posts").flatMap { response in
+        return try req.client().get("\(FrontEndRoutes.apiEndpoint())/posts").flatMap { response in
             let posts = try response.content.syncDecode([Post].self)
             let context = [ "posts": posts ]
             return try req.view().render("posts/index", context)
@@ -12,7 +12,7 @@ final class PostViewController {
     func show(_ req: Request) throws -> Future<View> {
         let postName = try req.parameters.next(String.self)
 
-        return try req.client().get("http://localhost:8080/api/posts/\(postName)").flatMap { response in
+        return try req.client().get("\(FrontEndRoutes.apiEndpoint())/posts/\(postName)").flatMap { response in
             let post = try response.content.syncDecode(Post.self)
             let context = [ "post": post ]
             return try req.view().render("posts/show", context)
--- Sources/App/FrontEnd/FrontEndRoutes.swift
+++ Sources/App/FrontEnd/FrontEndRoutes.swift
@@ -9,4 +9,8 @@ class FrontEndRoutes: RouteCollection {
         router.get("posts", use: postViewController.index)
         router.get("posts", String.parameter, use: postViewController.show)
     }
+
+    static func apiEndpoint() -> String {
+        return Environment.get("API_ENDPOINT") ?? "http://localhost:8080/api"
+    }
 }
