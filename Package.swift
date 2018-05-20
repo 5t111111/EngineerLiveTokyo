bb3445f615d9b5544815e52d16b1f1db45881dac
Hirofumi Wakasugi <baenej@gmail.com>
2018-05-20 11:40:42 +0900

    Add Markdown package to dependencies

--- Package.swift
+++ Package.swift
@@ -4,14 +4,12 @@ import PackageDescription
 let package = Package(
     name: "EngineerLiveTokyo",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🍃 Leaf template engine.
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.3"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.2"),
+        .package(url: "https://github.com/crossroadlabs/Markdown.git", .exact("1.0.0-alpha.2"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "Leaf"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "Markdown"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
